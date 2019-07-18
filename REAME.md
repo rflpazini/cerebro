3022221be6db6bcdf0689a2cae5d31191aeb0c5d
Rafael Pazini <rflpazini@gmail.com>
2019-07-18 18:13:20 -0300

    add: create lint, add readme, create travis pipeline and add codecoverage

---
+++ REAME.md
@@ -0,0 +1,3 @@
+# Cerebro
+
+Cerebro is an API that's consult taxpayer's central registry a.k.a. [Cadastro Centralizado de Contribuinte (CCC)](https://dfe-portal.svrs.rs.gov.br/Cte/Ccc)
---
+++ test/router/index.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Basic routes', function() {
+  test('should return 200 ping route', done => {
+    request(app)
+      .get('/ping')
+      .end((err, res) => {
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual('pong');
+        done();
+      });
+  });
+});
