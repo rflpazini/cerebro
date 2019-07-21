46ee7abfd0d532334a03e69c41f7421cbfba820c
Rafael Pazini <rflpazini@gmail.com>
2019-07-20 23:33:23 -0300

    add: create comments on test

--- test/utils/TaxPayerUtil.test.js
+++ test/utils/TaxPayerUtil.test.js
@@ -2,12 +2,18 @@ const TaxPayerUtil = require('../../src/utils/TaxPayerUtil');
 
 describe('Tax payer util', () => {
   test('should validate if data is valid (contain a class word)', () => {
+    //given
     const mockedPhrase = 'This phrase contains <class> word';
+
+    //then
     expect(TaxPayerUtil.isInvalidData(mockedPhrase)).toBe(false);
   });
 
   test('should validate if data has returned a content with the word class', () => {
+    //given
     const mockedPhrase = 'Not valid path';
+
+    //then
     expect(TaxPayerUtil.isInvalidData(mockedPhrase)).toBe(true);
   });
 });
